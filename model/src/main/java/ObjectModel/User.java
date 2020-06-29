9f6c17cb3731064602a1c1babfb687c702b133a6
Kostrula <d.galkin2011@yandex.ru>
2020-06-29 14:43:04 +0300

    Create base for application and 3 classes: User, Card, Account

---
+++ model/src/main/java/ObjectModel/User.java
@@ -0,0 +1,48 @@
+package ObjectModel;
+
+public class User {
+    private Long id;
+    private String login;
+    private String password;
+    private String sirName;
+    private String firstName;
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public void setLogin(String login){
+        this.login = login;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setSirName(String sirName){
+        this.sirName = sirName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getLogin(){
+        return login;
+    }
+    public String getPassword(){
+        return password;
+    }
+
+    public String getSirName() {
+        return sirName;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+}
